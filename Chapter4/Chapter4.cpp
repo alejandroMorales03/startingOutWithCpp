57434e6d286103e39059802b3fbc4a2e804fdf08
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2023-09-16 10:33:01 +0000

    UPDATE

--- Chapter4/Chapter4.cpp
+++ Chapter4/Chapter4.cpp
@@ -11,8 +11,9 @@ int main(){
         std::cout << "\n 1. Minimum/Maximum\n 2. Roman Numeral Calculator\n 3. Magic Dates";
         std::cout << "\n 4. Areas of Rectangles\n 5. Body Mass Index\n 6. Mass and Weight";
         std::cout << "\n 7. Time Calculator \n 8. Color Mixer \n 9. Change for a Dollar Game";
-        std::cout << "\n10. Days in a Month \n11. Math Totor\n12. Software Sales";
-        std::cout << "\n13. Book Club Points\n14. Bank Charges";
+        std::cout << "\n10. Days in a Month \n11. Math Tutor\n12. Software Sales";
+        std::cout << "\n13. Book Club Points\n14. Bank Charges\n15. Shipping Charges";
+        std::cout << "\n16. Running the Race\n17. Personal Best\n18. Fat Gram Calculator";
         std::cout << "\n\nSelection: ";
         std::cin >> choice;
 
@@ -257,18 +258,268 @@ int main(){
                 break;
             }
             case 12:{
-                const float PRICE = 99,
-                            DISCOUNTS_RATES[] = {0.20, 0.30, 0.40, 0.50};
-                            DISCOUNT_LIMITS[] = {10, 19, 49, 99 }
-                short quantity;
+            
+                short sold_units;
+                
                 do{
-                    std::cout << "\nEnter purchase quantity: ";
-                    std::cin >> quantity;
-                    if(quantity < 1)
-                        std::cout << "\nThere must be at least one item in the purchase\n";
-                }while(quantity < 1);
+                    std::cout << "\nEnter how many units were sold: ";
+                    std::cin >> sold_units;
+                    if(sold_units < 1)
+                        std::cout << "\nAt least one unit must be sold to complete the transaction\n";
+                }while(sold_units < 1);
 
+                const float DISCOUNTS[] = {.50, .40, .30, .20};
+                const float LOW_LIMITS[] = {100, 50, 20, 10};
+                const float RETAIL_PRICE = 99;
+                float final_price = sold_units * RETAIL_PRICE;
+                bool found = false;
 
+                for(short index = 0; index < sizeof(DISCOUNTS)/sizeof(DISCOUNTS[0]) && !found; index++){
+                    if(sold_units >= LOW_LIMITS[index]){
+                        found = true;
+                        final_price *= 1 - DISCOUNTS[index];
+                    }
+                }
+
+                std::cout << "\nFinal Cost: $ " << final_price;
+                break; 
+
+            }
+            case 13:{
+                const short BOOKS[] = {4, 3, 2, 1, 0};
+                const short POINTS[] = {60, 30, 15, 5, 0};
+                short read;
+
+                do{
+                    std::cout << "\nHow many books did you read: ";
+                    std::cin >> read;
+                    if(read < 0)
+                        std::cout << "\nInvalid Input\n";
+                }while(read < 0);
+
+                bool found = false;
+                for(short index = 0; !found && index < sizeof(BOOKS)/ sizeof(BOOKS[0]); index++){
+                    if(read >= BOOKS[index]){
+                        found = true;
+                        std::cout << "\n" << POINTS[index] << " points";
+                    }
+                }
+                break;
+            }
+            case 14:{
+                const float MONTHLY_FEE = 10, BAL_UNDER_400_FEE = 15;
+                const float CHECK_FEES[] = {0.04, 0.06, 0.08, .10};
+                const short CHECKS[] = {60, 40, 20, 0};
+
+                float balance, fees = 10;;
+                short checks;
+                bool found = false;
+
+                do{
+                    std::cout << "\nEnter current balance: ";
+                    std::cin >> balance;
+                    std::cout << "Enter number of checks written: ";
+                    std::cin >> checks;
+                    if(checks < 0)
+                        std::cout << "\nINVALID INPUT FOR CHECKS FIELD\n";
+                }while(checks < 0);
+
+                if(balance< 400){
+                    fees += 15;
+                    if(fees < 0)
+                        std::cout << "\nAccount is overdrawn";
+                }
+          
+                for(short index = 0; !found && index < sizeof(CHECK_FEES)/sizeof(CHECK_FEES[0]); index++){
+                    if(checks >= CHECKS[index]){
+                        found = true;
+                        fees += checks * CHECK_FEES[index];
+                    }
+                }
+
+                std::cout << "\nMonthly Service Fee: $ " << fees;
+                break;
+            }
+            case 15:{
+                float weight, miles, price;
+                const float LOWER_LIMITS[] = {10, 6, 2, 0};
+                const float RATES[] = {4.80, 3.70, 2.20, 1.10};
+
+                bool found = false;
+
+                do{
+                    std::cout << "\nEnter the weight of the package in kg: ";
+                    std::cin >> weight;
+                    std::cout << "Enter the distance in miles: ";
+                    std::cin >> miles;
+                    if(!(weight <= 20 && weight > 0 && miles >= 10 && miles <= 3000)){
+                        std::cout << "\nWEIGHT MUST BE WITHIN 0 - 20 kg (0 not inclusive)";
+                        std::cout << "\nDISTANCE MUST BE WITHIN 10 - 3000 miles\n";
+                    }
+                }while(!(weight <= 20 && weight > 0 && miles >= 10 && miles <= 3000));
+
+                miles /=500;
+                for(short index = 0;!found && index < sizeof(LOWER_LIMITS)/sizeof(LOWER_LIMITS[0]); index++){
+                    if(weight > LOWER_LIMITS[index]){
+                        price = miles * RATES[index];
+                        found = true;
+                    }
+                }
+                std::cout << "\nTotal: $ " << price;
+                break;
+            }
+            case 16:{
+                short time_one, time_two, time_three;
+                do{
+                    std::cout << "\nEnter the times for thre runners in seconds: ";
+                    std::cin >> time_one >> time_two >> time_three;
+                    if(time_one < 0 || time_two < 0 || time_three < 0)
+                        std::cout << "\nAll times must be positive\n";
+                }while(time_one < 0 || time_two < 0 || time_three < 0);
+
+                if(time_one < time_two && time_one < time_three){
+                    std::cout << "\nFirst place: Runner 1";
+                    if(time_two < time_three){
+                        std::cout << "\nSecond place: Runner 2";
+                        std::cout << "\nThird place: Runner 3";
+                    }
+                    else{
+                        std::cout << "\nSecond place: Runner 3";
+                        std::cout << "\nThird place: Runner 2";
+                    }
+                }
+
+                if(time_two < time_one && time_two < time_three){
+                    std::cout << "\nFirst place: Runner 2";
+                    if(time_one < time_three){
+                        std::cout << "\nSecond place: Runner 1";
+                        std::cout << "\nThird place: Runner 3";
+                    }
+                    else{
+                        std::cout << "\nSecond place: Runner 3";
+                        std::cout << "\nThird place: Runner 1";
+                    }
+                }
+         
+                if(time_three < time_one && time_three < time_two){
+                    std::cout << "\nFirst place: Runner 3";
+                    if(time_two < time_one){
+                        std::cout << "\nSecond place: Runner 2";
+                        std::cout << "\nThird place: Runner 1";
+                    }
+                    else{
+                        std::cout << "\nSecond place: Runner 1";
+                        std::cout << "\nThird place: Runner 2";
+                    }
+                }
+                break;
+            }
+            case 17:{
+                std::string dates[3];
+                float heights[3];
+
+                for(short index = 0; index < 3; index++){
+                    std::cout << "\nEnter date (Month DD, YYYY): ";
+                    std::cin.ignore();
+                    getline(std::cin, dates[index]);
+                    do{
+                        std::cout << "Enter height in meters: ";
+                        std::cin >> heights[index];
+
+                        if(heights[index] < 2 || heights[index] > 5)
+                            std::cout << "\nHeight must be within 2 and 5\n\n";
+                    }while(heights[index] < 2 || heights[index] > 5);    
+                }
+                
+                bool swap = false;
+                float temp;
+                std::string temp_s;
+                do{
+                    swap = false;
+                    for(short index = 0; index < 2; index++){
+                        if(heights[index] < heights[index + 1]){
+                            temp = heights[index];
+                            heights[index] = heights[index + 1];
+                            heights[index + 1] = temp;
+
+                            temp_s = dates[index];
+                            dates[index] = dates[index + 1];
+                            dates[index + 1] = temp_s;
+                            swap = true;
+                        }
+                    }
+
+                }while(swap);
+
+                for(short index = 0; index < 3; index++){
+                    std::cout << "\n" << dates[index] << ": " << heights[index] << " meters";
+                }
+                break;
+            
+            }
+            case 18:{
+                short calories;
+                float fat_grams;
+                do{
+                    std::cout << "\nEnter total calories: ";
+                    std::cin >> calories;
+                    std::cout << "\nEnter fat grams: ";
+                    std::cin >> fat_grams;
+                    if(calories < 0 || fat_grams < 0){
+                        std::cout << "\nCalories and fat grams must be positive numbers\n";
+                    }
+
+                }while(calories < 0 || fat_grams < 0);
+
+                float cal_from_fat = fat_grams * 9, fat_cal_pctage = cal_from_fat/calories * 100;
+                
+                if(cal_from_fat > calories)
+                    std::cout << "\nYou must have entered either the total calories or the gram fats incorrectly!";
+                else
+                    std::cout << "\n" << fat_cal_pctage << " percent of the calories come from fat. " << (fat_cal_pctage < 30? " That food is low in fat!" : "");
+                break;
+            }
+            case 19:{
+                bool found = false;
+                float user_wavelength;
+                const float WAVELENGTHS[7] = {1e-11, 1e-8, 4e-7, 7e-7, 1e-3, 1e-2, };
+                const std::string Names[7] = {"Gamma Rays", "X Rays", "Ultraviolet", "Visible Light", "Infrared", "Microwaves", "Radio Waves"};
+
+                do{
+                    std::cout << "\nEnter a wavelength in meters: ";
+                    std::cin >> user_wavelength;
+                    if(user_wavelength < 0)
+                        std::cout << "\nWavelengths must be positive\n";
+                }while(user_wavelength < 0);
+                
+                for(short index = 0; !found && index < 7; index++){
+                    if(user_wavelength <= WAVELENGTHS[index]){
+                        std::cout << "\n" << Names[index];
+                        found = true;
+                    }
+                }
+                if(!found)
+                    std::cout << Names[6];
+                break;
+            }
+            case 20:{
+                short selection;
+                float distance;
+                const short SPEEDS[] = {1100, 4900, 16400};
+                do{
+                    std::cout << "\nSelect a medium:\n\n1. Air\n2. Water\n3. Steel";
+                    std::cout << "\n\nSelection: ";
+                    std::cin >> selection;
+                    std::cout << "Enter a distance in feet: ";
+                    std::cin >> distance;
+                    if(selection < 1 || selection > 3||distance < 0)
+                        std::cout << "\nINVALID INPUT\n";
+                }while(selection < 1 || selection > 3||distance < 0);
+                
+                std::cout << "Time: " << distance * 1/SPEEDS[selection - 1];
+                 break;
+       
+                
             }
             default:
                 std::cout << "\nINVALID INPUT";
